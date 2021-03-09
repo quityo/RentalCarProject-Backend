64f9c82345df763f6bb24bdecf5b2e998f618ca2
quityo <64424786+quityo@users.noreply.github.com>
2021-03-09 05:25:55 +0300

    fix

--- DataAccess/Abstract/IUserDal.cs
+++ DataAccess/Abstract/IUserDal.cs
@@ -1,12 +1,15 @@
 ﻿using Core.DataAccess;
 using Core.Entities.Concrete;
 using Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace DataAccess.Abstract
 {
     public interface IUserDal:IEntityRepository<User>
     {
         List<OperationClaim> GetClaims(User user);
+        //List<UserDetailDto> GetUserDetails(Expression<Func<User, bool>> filter = null);
     }
 }
